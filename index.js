e8d1a6f7e577a77103d324aebed7f2b9e7912003
muhammadumar0820 <salimjonovmuhammadumar372@gmail.com>
2024-01-10 18:54:30 +0300

    masalalar ishlandi

---
+++ index.js
@@ -0,0 +1,41 @@
+//Birinchi masala
+//function Element(arg1, arg2) {
+//    let sum = 0;
+//    for (let i = 0; i < arg1.length; i++) {
+//      if (typeof arg1[i] == 'number' && arg2.includes(arg1[i])) {
+//        sum += arg1[i];
+//      }
+//      arg1 = [1, 2, "salom", true, "xayr"]
+//      arg2 = [1, 3, "hello", false, "bye"]
+//    }
+//    console.log(sum);
+//    return sum;
+//}
+
+//Ikkinchi masala
+//function capitalizeWords(text) {
+//    let words = text.split('lorem ipsum');
+//      for (let i = 0; i < words.length; i++) {
+//      let word = words[i];
+//      words[i] = word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+//    }
+//      return words.join('lorem ipsum');
+//  }
+//  capitalizeWords("lorem ipsum")
+
+//Uchinchi masala
+//function Palindrom(text) {
+//    let cleanText = text.toLowerCase().replace('');
+//      let head = 0;
+//    let foot = cleanText.length - 1;
+//    while (head < foot) {
+//      if (cleanText[head] !== cleanText[foot]) {
+//        return false;
+//      }
+//        head++;
+//        foot--;
+//    }
+//   return true;
+//}
+
+//tortinchi masala
